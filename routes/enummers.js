45b60a07a913c39706faca00e2924120670b5b1c
slagveer <patric.slagveer@gmail.com>
2013-02-27 00:28:00 +0100

    Soorten en Categorieen

---
+++ routes/enummers.js
@@ -0,0 +1,94 @@
+/*
+ * GET users listing.
+ */
+var mysql = require('mysql');
+
+exports.categorieen = function(req, res){
+
+    var connection = mysql.createConnection({
+        user: "root",
+        password: "root",//"webdesign",
+        database: "enummers",
+        host: '127.0.0.1',
+        port: '8889'//'3306'
+    });
+    req.on('end', function () {
+        var host = req.headers.host;
+        console.log(host);
+        if(host == '109.235.76.92:3000' || host == 'localhost:3000' || host == '127.0.0.1:3000'){
+            // Query the database.
+            connection.query('SELECT * FROM categorieen;', function (error, rows, fields) {
+                res.writeHead(200, {
+                    "Content-Type": "application/json",
+                    "Access-Control-Allow-Origin": "*"
+                });
+                //res.end('__parseJSONPResponse(' + JSON.stringify(rows) + ')');
+                res.end(JSON.stringify(rows));
+            });
+        }
+        else{
+            res.send("Restriction, sorry!!!" + host);
+        }
+    });
+    //res.send("respond with a resource");
+};
+
+exports.soorten = function(req, res){
+
+    var connection = mysql.createConnection({
+        user: "root", 
+        password: "root",//"webdesign",
+        database: "enummers",
+        host: '127.0.0.1',
+        port: '8889'//'3306'
+    });
+    req.on('end', function () {
+        var host = req.headers.host;
+        console.log(host);
+        if(host == '109.235.76.92:3000' || host == 'localhost:3000' || host == '127.0.0.1:3000'){
+            // Query the database.
+            connection.query('SELECT * FROM soorten;', function (error, rows, fields) {
+                res.writeHead(200, {
+                    "Content-Type": "application/json",
+                    "Access-Control-Allow-Origin": "*"
+                });
+                //res.end('__parseJSONPResponse(' + JSON.stringify(rows) + ')');
+                res.end(JSON.stringify(rows));
+            });
+        }
+        else{
+            res.send("Restriction, sorry!!!" + host);
+        }
+    });
+    //res.send("respond with a resource");
+};
+
+exports.enummers = function(req, res){
+
+    var connection = mysql.createConnection({
+        user: "root",
+        password: "root",//"webdesign",
+        database: "enummers",
+        host: '127.0.0.1',
+        port: '8889'//'3306'
+    });
+    req.on('end', function () {
+        var host = req.headers.host;
+        console.log(host);
+        if(host == '109.235.76.92:3000' || host == 'localhost:3000' || host == '127.0.0.1:3000'){
+            // Query the database.
+            connection.query('SELECT * FROM enummers;', function (error, rows, fields) {
+                res.writeHead(200, {
+                    "Content-Type": "application/json",
+                    "Access-Control-Allow-Origin": "*"
+                });
+                //res.end('__parseJSONPResponse(' + JSON.stringify(rows) + ')');
+                res.end(JSON.stringify(rows));
+            });
+        }
+        else{
+            res.send("Restriction, sorry!!!" + host);
+        }
+    });
+    //res.send("respond with a resource");
+};
